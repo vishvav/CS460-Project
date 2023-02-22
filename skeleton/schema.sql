09a3e360cd139cd5cd7e7bcc6a5f49968fe4893f
vishvav <115579820+vishvav@users.noreply.github.com>
2023-02-22 13:58:31 -0500

    Update schema.sql
    Update schema.sql

--- skeleton/schema.sql
+++ skeleton/schema.sql
@@ -4,20 +4,83 @@ USE photoshare;
 
 
 CREATE TABLE Users (
-    user_id int4  AUTO_INCREMENT,
-    email varchar(255) UNIQUE,
-    password varchar(255),
-  CONSTRAINT users_pk PRIMARY KEY (user_id)
+    user_id INT NOT NULL AUTO_INCREMENT,
+    gender VARCHAR(6),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    dob DATE NOT NULL,
+    hometown VARCHAR(255),
+    fname VARCHAR(255) NOT NULL,
+    lname VARCHAR(255) NOT NULL,
+    PRIMARY KEY (user_id)
 );
 
-CREATE TABLE Pictures
-(
-  picture_id int4  AUTO_INCREMENT,
-  user_id int4,
-  imgdata longblob ,
-  caption VARCHAR(255),
-  INDEX upid_idx (user_id),
-  CONSTRAINT pictures_pk PRIMARY KEY (picture_id)
+CREATE TABLE Albums (
+  album_id INT AUTO_INCREMENT,
+  Name VARCHAR(255) NOT NULL,
+  date_of_creation DATENAME DEFAULT CURRENT_TIMESTAMP,
+  user_id INT NOT NULL,
+  PRIMARY KEY (album_id),
+  FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
 );
+
+
+CREATE TABLE Pictures(
+  picture_id INT AUTO_INCREMENT,
+  user_id INT,
+  caption VARCHAR(255),
+  imgdata LONGBLOB,
+  album_id INT NOT NULL,
+  PRIMARY KEY (picture_id),
+  FOREIGN KEY (user_id) REFERENCES Users(user_id),
+  FOREIGN KEY (album_id) REFERENCES Albums(album_id) ON DELETE CASCADE
+);
+
+CREATE TABLE Comments(
+  comment_od INT NOT NULL AUTO_INCREMENT,
+  text TEXT NOT NULL,
+  date DATETIME DEFAULT CURRENT_TIMESTAMP,
+  user_id INT NOT NULL,
+  picture_id INT NOT NULL,
+  PRIMARY KEY (comment_od),
+  FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+  FOREIGN KEY (picture_id) REFERENCES Pictures(picture_id) ON DELETE CASCADE
+
+);
+
+CREATE TABLE Likes(
+  user_id INT NOT NULL,
+  picture_id INT NOT NULL,
+  PRIMARY KEY (user_id, picture_id),
+  FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+  FOREIGN KEY (picture_id) REFERENCES Pictures(picture_id) ON DELETE CASCADE
+);
+
+CREATE TABLE Tags(
+  tag_id INT NOT NULL,
+  name VARCHAR(255),
+  PRIMARY KEY (tag_id)
+);
+
+CREATE TABLE Tagged(
+  photo_id INT,
+  tag_id INT,
+  PRIMARY KEY (photo_id, tag_id),
+  FOREIGN KEY (photo_id) REFERENCES Pictures(picture_id),
+  FOREIGN KEY (tag_id) REFERENCES Tags(tag_id)
+);
+
+CREATE TABLE Friendship(
+  UID1 INT NOT NULL,
+  UID2 INT NOT NULL,
+  CHECK (UID1 <> UID2),
+  PRIMARY KEY (UID1, UID2),
+  FOREIGN KEY (UID1) REFERENCES Users(user_id) ON DELETE CASCADE,
+  FOREIGN KEY (UID2) REFERENCES Users(user_id) ON DELETE CASCADE
+);
+
+CREATE ASSERTION Comment-Constraint CHECK 
+(NOT EXISTS (SELECT * FROM Comments C, Pictures P WHERE C.picture_id = P.picture_id AND P.user_id = C.user_id));
+
 INSERT INTO Users (email, password) VALUES ('test@bu.edu', 'test');
 INSERT INTO Users (email, password) VALUES ('test1@bu.edu', 'test');
